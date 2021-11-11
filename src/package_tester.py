c1e2be7f32ab014a6bbb5fabe0bbad229eaf8e7f
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-11 11:35:38 +0200

    create calendar and test event

--- src/package_tester.py
+++ src/package_tester.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# uses the icalendar module to generate an iCalendar file
 from icalendar import Calendar, Event
 import pytz
 from datetime import datetime
@@ -8,9 +9,24 @@ from pathlib import Path
 
 import icalendar as ical
 
-
+# check if the imported modules are working
 print(ical.__version__)
 print(Calendar.__name__)
 
 
 cal = Calendar()
+# Set ourselves as the calendar's owner, required by most servers
+cal.add('attendee', 'MAILTO:robert.poenaru@icloud.com')
+
+event = Event()
+event.add('summary', 'Test Event')
+event.add('dtstart', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
+event.add('dtend', datetime(2019, 1, 1, 11, 0, 0, tzinfo=pytz.UTC))
+event.add('dtstamp', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
+event.add('description', 'This is a test event')
+
+
+# add event to calendar
+cal.add_component(event)
+
+print(cal)
