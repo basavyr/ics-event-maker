357e21ad9fed6a7772d9e157255d0093eeac3831
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-11 11:49:21 +0200

    implement the tester into a dedicated function

--- src/package_tester.py
+++ src/package_tester.py
@@ -14,19 +14,35 @@ print(ical.__version__)
 print(Calendar.__name__)
 
 
-cal = Calendar()
+# cal = Calendar()
 # Set ourselves as the calendar's owner, required by most servers
-cal.add('attendee', 'MAILTO:robert.poenaru@icloud.com')
+# cal.add('attendee', 'MAILTO:robert.poenaru@icloud.com')
 
-event = Event()
-event.add('summary', 'Test Event')
-event.add('dtstart', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
-event.add('dtend', datetime(2019, 1, 1, 11, 0, 0, tzinfo=pytz.UTC))
-event.add('dtstamp', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
-event.add('description', 'This is a test event')
+# event = Event()
+# event.add('summary', 'Test Event')
+# event.add('dtstart', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
+# event.add('dtend', datetime(2019, 1, 1, 11, 0, 0, tzinfo=pytz.UTC))
+# event.add('dtstamp', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
+# event.add('description', 'This is a test event')
 
 
-# add event to calendar
-cal.add_component(event)
+# # add event to calendar
+# cal.add_component(event)
 
-print(cal)
+# print(cal)
+
+# print(os.getcwd())
+
+# with open('test.ics', 'wb') as f:
+#     f.write(cal.to_ical())
+
+
+def Create_iCal_Event(start, end, stamp, summary, description):
+    event = Event()
+    event.add('summary', summary)
+    event.add('dtstart', start)
+    event.add('dtend', end)
+    event.add('dtstamp', stamp)
+    event.add('description', description)
+    cal.add_component(event)
+    return cal
