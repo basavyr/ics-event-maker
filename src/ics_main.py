eb999755f01cf102753c27ee40060d8092615499
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-13 12:29:25 +0200

    Update ics_main.py

--- src/ics_main.py
+++ src/ics_main.py
@@ -3,7 +3,6 @@
 # local imports
 import day_generator as day_gen
 
-
 nov_first_work_day = 15
 dec_first_work_day = 1
 
@@ -13,9 +12,26 @@ dec_last_day = 31
 nov_month = [x for x in range(1, nov_last_day + 1)]
 dec_month = [x for x in range(1, dec_last_day + 1)]
 
+# generate the days for RT sessions for the month of November
 nov_work_days = day_gen.generate_month_work_days(
     nov_first_work_day, nov_month)
+# generate the days for RT sessions for the month of December
 dec_work_days = day_gen.generate_month_work_days(
     dec_first_work_day, dec_month)
-print(nov_work_days)
-print(dec_work_days)
+
+
+# Define calendar parameters
+EMAIL = 'robert.poenaru@icloud.com'
+
+# Generate a set of dates for the calendar
+DESCRIPTION = "Radiotherapy sessions | B3"
+SUMMARY = lambda id: f'☢️ RT Session-{id}'
+YEAR = 2021
+M_NOV = 11
+M_DEC = 12
+D_NOV = [x for x in range(15, 31)]
+D_DEC = [x for x in range(1, 32)]
+
+# define the RT start time and end time
+START_SESSION = [11, 36, 0]
+END_SESSION = [12, 30, 0]
