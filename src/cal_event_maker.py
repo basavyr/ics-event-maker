0d131c9c91d4396465080dbb2e41453df768d136
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-13 12:29:19 +0200

    update the event maker with datetime function

--- src/cal_event_maker.py
+++ src/cal_event_maker.py
@@ -7,11 +7,44 @@ from datetime import datetime
 import os
 from pathlib import Path
 
-nov_first_work_day = 15
-dec_first_work_day = 1
-nov_end_day = 30
-dec_end_day = 31
 
-nov_month_days = [x for x in range(1, nov_end_day + 1)]
-dec_month_days = [x for x in range(1, dec_end_day + 1)]
+def CreateDateTime(year, month, day, hour, minute, second):
+    """generate a proper datetime that will ve used in the icalendar file"""
+    return datetime(year, month, day, hour, minute, second, tzinfo=pytz.UTC)
 
+
+def Create_iCal_Event(start, end, stamp, summary, description):
+    event = Event()
+    event.add('summary', summary)
+    event.add('dtstart', start)
+    event.add('dtend', end)
+    event.add('dtstamp', stamp)
+    event.add('description', description)
+    cal.add_component(event)
+    return cal
+
+
+print(CreateDateTime(2020, 1, 1, 0, 0, 0))
+
+
+# cal = Calendar()
+# Set ourselves as the calendar's owner, required by most servers
+# cal.add('attendee', 'MAILTO:robert.poenaru@icloud.com')
+
+# event = Event()
+# event.add('summary', 'Test Event')
+# event.add('dtstart', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
+# event.add('dtend', datetime(2019, 1, 1, 11, 0, 0, tzinfo=pytz.UTC))
+# event.add('dtstamp', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
+# event.add('description', 'This is a test event')
+
+
+# # add event to calendar
+# cal.add_component(event)
+
+# print(cal)
+
+# print(os.getcwd())
+
+# with open('test.ics', 'wb') as f:
+#     f.write(cal.to_ical())
