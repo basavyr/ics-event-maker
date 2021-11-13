74f67eb0b6f0c8ac5255e9605ecd7f47fa87d93f
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-13 12:37:27 +0200

    cleaned the event maker

--- src/cal_event_maker.py
+++ src/cal_event_maker.py
@@ -20,31 +20,4 @@ def Create_iCal_Event(start, end, stamp, summary, description):
     event.add('dtend', end)
     event.add('dtstamp', stamp)
     event.add('description', description)
-    cal.add_component(event)
-    return cal
-
-
-print(CreateDateTime(2020, 1, 1, 0, 0, 0))
-
-
-# cal = Calendar()
-# Set ourselves as the calendar's owner, required by most servers
-# cal.add('attendee', 'MAILTO:robert.poenaru@icloud.com')
-
-# event = Event()
-# event.add('summary', 'Test Event')
-# event.add('dtstart', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
-# event.add('dtend', datetime(2019, 1, 1, 11, 0, 0, tzinfo=pytz.UTC))
-# event.add('dtstamp', datetime(2019, 1, 1, 10, 0, 0, tzinfo=pytz.UTC))
-# event.add('description', 'This is a test event')
-
-
-# # add event to calendar
-# cal.add_component(event)
-
-# print(cal)
-
-# print(os.getcwd())
-
-# with open('test.ics', 'wb') as f:
-#     f.write(cal.to_ical())
+    return event
