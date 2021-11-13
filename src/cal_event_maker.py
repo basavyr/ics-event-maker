c3a4416ec0bc77cce38161ecce8f4d9d7e015117
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-13 13:23:42 +0200

    Update cal_event_maker.py

--- src/cal_event_maker.py
+++ src/cal_event_maker.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 def CreateDateTime(year, month, day, hour, minute, second):
     """generate a proper datetime that will ve used in the icalendar file"""
+    # https://stackoverflow.com/questions/35351774/python-datetime-conversions-from-one-local-timezone-to-another-bonus-dsts
     return datetime(year, month, day, hour, minute, second, tzinfo=timezone('Europe/Bucharest'))
 
 
@@ -21,5 +22,7 @@ def Create_iCal_Event(start, end, stamp, summary, description, location):
     event.add('dtend', end)
     event.add('dtstamp', stamp)
     event.add('description', description)
+    # https://www.programcreek.com/python/example/56510/icalendar.Event
+    # https://icalendar.readthedocs.io/en/latest/cli.html?highlight=location
     event.add('location', location)
     return event
