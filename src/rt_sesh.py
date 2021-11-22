e1197e17901f10f2f255347ac3715bc1122e8aa1
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-22 14:46:13 +0000

    add test for RT session maker

--- src/rt_sesh.py
+++ src/rt_sesh.py
@@ -1,18 +1,36 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 class Sessions():
+    """For each session type, request the initial date and the number of sessions that are available as input"""
+
+    @staticmethod
+    def DateFormat(date):
+        return f'{date}.12.2021'
+
+
     # generate the rt sessions
     @staticmethod
-    def RT_Sessions(start_date,end_date):
-        return 1,1
+    def RT_Sessions(start_date,n_sesh):
+        sessions=[]
+        current_date=start_date
+        for id in range(n_sesh):
+            sessions.append(Sessions.DateFormat(current_date))
+            current_date=current_date+7
+        return sessions
     
     # generate the nutritional sessions
     @staticmethod
-    def Nutrition_Session(start_date, end_date):
+    def Nutrition_Session(start_date, n_sesh):
         return 1,1
 
     # generate the toxicology sessions
     @staticmethod
-    def Toxicology_Session(start_date,end_date):
+    def Toxicology_Session(start_date,n_sesh):
         return 1,1
+
+
+
+print(Sessions.RT_Sessions(1,4))
+
+
