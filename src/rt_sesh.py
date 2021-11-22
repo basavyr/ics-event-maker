0d132c679adf5561eabdd701a98ba23033a6d0cb
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-22 14:32:35 +0000

    add session class for creating events

---
+++ src/rt_sesh.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Sessions():
+    # generate the rt sessions
+    @staticmethod
+    def RT_Sessions(start_date,end_date):
+        return 1,1
+    
+    # generate the nutritional sessions
+    @staticmethod
+    def Nutrition_Session(start_date, end_date):
+        return 1,1
+
+    # generate the toxicology sessions
+    @staticmethod
+    def Toxicology_Session(start_date,end_date):
+        return 1,1
