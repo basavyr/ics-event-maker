3f193401ffb2093ceb80fb7658afee9d276bcb4d
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-24 19:43:21 +0200

    adds nutrition visits

--- src/rt_sesh.py
+++ src/rt_sesh.py
@@ -75,29 +75,45 @@ for day in range(RT_START_DATE, 31, 7):
     RT_DAYS_START.append(rt_day_0)
     RT_DAYS_END.append(rt_day_1)
 
-NUTRITION_DAYS = []
+
+NUTRITION_DAYS_START = []
+NUTRITION_DAYS_END = []
 # show the dates for nutrition
 for day in range(NUTRITION_START_DATE, 31, 7):
-    nutrition_day = event_maker.CreateDateTime(YEAR, M_DEC, day,
-                                               NUTRITION_START_HOUR[0], NUTRITION_START_HOUR[1], NUTRITION_START_HOUR[2])
-    NUTRITION_DAYS.append(nutrition_day)
+    nutrition_day_0 = event_maker.CreateDateTime(YEAR, M_DEC, day,
+                                                 NUTRITION_START_HOUR[0], NUTRITION_START_HOUR[1], NUTRITION_START_HOUR[2])
+    nutrition_day_1 = event_maker.CreateDateTime(YEAR, M_DEC, day,
+                                                 NUTRITION_START_HOUR[0] + 1, NUTRITION_START_HOUR[1], NUTRITION_START_HOUR[2])
+    NUTRITION_DAYS_START.append(nutrition_day_0)
+    NUTRITION_DAYS_END.append(nutrition_day_1)
 
 
-TOXIC_DAYS = []
+TOXIC_DAYS_START = []
+TOXIC_DAYS_END = []
 # show the dates for toxicology
 for day in range(TOXIC_START_DATE, 31, 7):
-    toxicology_day = event_maker.CreateDateTime(YEAR, M_DEC, day,
-                                                TOXIC_START_HOUR[0], TOXIC_START_HOUR[1], TOXIC_START_HOUR[2])
-    TOXIC_DAYS.append(toxicology_day)
+    toxicology_day_0 = event_maker.CreateDateTime(YEAR, M_DEC, day,
+                                                  TOXIC_START_HOUR[0], TOXIC_START_HOUR[1], TOXIC_START_HOUR[2])
+    toxicology_day_1 = event_maker.CreateDateTime(YEAR, M_DEC, day,
+                                                  TOXIC_START_HOUR[0] + 1, TOXIC_START_HOUR[1], TOXIC_START_HOUR[2])
+    TOXIC_DAYS_START.append(toxicology_day_0)
+    TOXIC_DAYS_END.append(toxicology_day_1)
 
 
 cal = package_tester.Calendar()
 # Set ourselves as the calendar's owner, required by most servers
 cal.add('attendee', f'MAILTO:{EMAIL}')
 
-for id in range(len(RT_DAYS_START)):
-    cal.add_component(event_maker.Create_iCal_Event(
-        RT_DAYS_START[id], RT_DAYS_END[id], STAMP, RT_DESCRIPTION, RT_DESCRIPTION, LOCATION))
+# for id in range(len(RT_DAYS_START)):
+#     cal.add_component(event_maker.Create_iCal_Event(
+#         RT_DAYS_START[id], RT_DAYS_END[id], STAMP, RT_DESCRIPTION, RT_DESCRIPTION, LOCATION))
 
-with open('RT_Visits.ics', 'wb') as f:
+# with open('RT_Visits.ics', 'wb') as f:
+#     f.write(cal.to_ical())
+
+for id in range(len(NUTRITION_DAYS_START)):
+    cal.add_component(event_maker.Create_iCal_Event(
+        NUTRITION_DAYS_START[id], NUTRITION_DAYS_END[id], STAMP, NUTRITION_DESCRIPTION, NUTRITION_DESCRIPTION, LOCATION))
+
+with open('NUTRITION_Visits.ics', 'wb') as f:
     f.write(cal.to_ical())
