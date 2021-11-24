68e7a1bbc156e0bb6af2c66f6f4b5093e0c50d3d
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-24 19:25:34 +0200

    Update rt_sesh.py

--- src/rt_sesh.py
+++ src/rt_sesh.py
@@ -1,6 +1,13 @@
 # import numpy as np
 # import matplotlib.pyplot as plt
 
+
+# local imports
+import day_generator as day_gen
+import cal_event_maker as event_maker
+import package_tester
+
+
 class Sessions():
     """For each session type, request the initial date and the number of sessions that are available as input"""
 
@@ -33,13 +40,32 @@ class Sessions():
 # Define calendar parameters
 EMAIL = 'robert.poenaru@icloud.com'
 
+# set the default location for the calendar events
+LOCATION = 'Amethyst Radiotherapy Calea Odăii, 44, Otopeni, 030171, Romania'
+YEAR = 2021
+M_NOV = 11
+M_DEC = 12
+STAMP = event_maker.datetime.utcnow()
 
-RT_START_DATE = 30
+# create datetime with the values given by the configured session constants
+# dt_start = lambda day, month: event_maker.CreateDateTime(
+#     YEAR, month, day, START_SESSION[0], START_SESSION[1], START_SESSION[2])
+
+# dt_end = lambda day, month: event_maker.CreateDateTime(
+# YEAR, month, day, END_SESSION[0], END_SESSION[1], END_SESSION[2])
+
+RT_START_DATE = 7
 RT_START_HOUR = [12, 10, 00]
-RT_DESCRIPTION = ''
-TOXIC_START_DATE = 26
+RT_DESCRIPTION = '🩺 Radiology Consultation | Dr. Zarma'
+TOXIC_START_DATE = 10
 TOXIC_START_HOUR = [12, 00, 00]
-TOXIC_DESCRIPTION = ''
-NUTRITION_START_DATE = 29
+TOXIC_DESCRIPTION = '☣️ Toxicity Consultation'
+NUTRITION_START_DATE = 6
 NUTRITION_START_HOUR = [12, 10, 00]
-NUTRITION_DESCRIPTION = ''
+NUTRITION_DESCRIPTION = '🥗 Nutrition Consultation'
+
+# show the dates for RT
+for day in range(RT_START_DATE, 31, 7):
+    rt_day = event_maker.CreateDateTime(YEAR, M_DEC, day,
+                                        RT_START_HOUR[0], RT_START_HOUR[1], RT_START_HOUR[2])
+    print(rt_day)
